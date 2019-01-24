1f48e1319517e0d2dfb658f3c1fc40aa7761ca86
zxyzxy1996 <zxyzxy1996>
2019-01-24 18:07:25 +0800

    InsertionSort Advance

--- src/com/imooc/algorithm/InsertionSort.java
+++ src/com/imooc/algorithm/InsertionSort.java
@@ -8,9 +8,27 @@ public class InsertionSort {
     public static void sort(Comparable arr[]) {
         int n = arr.length;
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j > 0; j--) {
+            //写法1
+//            for (int j = i; j > 0; j--) {
+//                if (arr[j].compareTo(arr[j - 1]) < 0) {
+//                    swap(arr, j, j - 1);
+//                } else {
+//                    break;
+//                }
+//            }
 
+            //写法2
+//            for (int j = i; j > 0 && arr[j].compareTo(arr[j - 1]) < 0; j--) {
+//                swap(arr, j, j - 1);
+//            }
+
+            //写法3
+            Comparable e = arr[i];
+            int j = i;
+            for (; j > 0 && arr[j - 1].compareTo(e) > 0; j--){
+                arr[j] = arr[j - 1];
             }
+            arr[j] = e;
         }
     }
 
@@ -18,5 +36,27 @@ public class InsertionSort {
         Object t = arr[i];
         arr[i] = arr[j];
         arr[j] = t;
+    }
+
+    public static void main(String[] args) {
+        Integer arr[] = {4, 3, 5, 6, 7, 9, 1, 10};
+        InsertionSort.sort(arr);
+
+        for (int num : arr) {
+            System.out.print(num);
+            System.out.print(' ');
+        }
+
+        System.out.println();
+
+        Double arr1[] = {4.1, 3.2, 5.5, 5.6, 5.7, 1.9, 1.6, 10.5};
+        InsertionSort.sort(arr1);
+
+        for (double num : arr1) {
+            System.out.print(num);
+            System.out.print(' ');
+        }
+
+        System.out.println();
     }
 }
