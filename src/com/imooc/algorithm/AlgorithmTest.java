be03415b972a136fda94ceff9070154f79f342e9
zxyzxy1996 <zxyzxy1996>
2019-01-29 14:52:30 +0800

    Study MergeSort

--- src/com/imooc/algorithm/AlgorithmTest.java
+++ src/com/imooc/algorithm/AlgorithmTest.java
@@ -6,20 +6,11 @@ public class AlgorithmTest {
         SortTestHelper sortTestHelper = new SortTestHelper();
         SelectionSort selectionSort = new SelectionSort();
         InsertionSort insertionSort = new InsertionSort();
+        MergeSort mergeSort = new MergeSort();
 
-//        Comparable arrSelection1[] = sortTestHelper.generateRandomArray(100000, 1, 100000);
-//        Comparable arrInsertion1[] = sortTestHelper.generateRandomArray(100000, 1, 100000);
-        Comparable arrSelection2[] = sortTestHelper.generateNearlyOrderedArray(100000, 1000);
-        Comparable arrInsertion2[] = sortTestHelper.generateNearlyOrderedArray(100000, 1000);
-//        double time1 = sortTest(selectionSort, arrSelection1);
-//        System.out.println("SelectionSort with randomArray: " + time1 + "s");
-//        double time2 = sortTest(insertionSort, arrInsertion1);
-//        System.out.println("InsertionSort with randomArray: " + time2 + "s");
-
-        double time3 = sortTest(selectionSort, arrSelection2);
-        System.out.println("SelectionSort with swapArray: " + time3 + "s");
-        double time4 = sortTest(insertionSort, arrInsertion2);
-        System.out.println("InsertionSort with swapArray: " + time4 + "s");
+        Comparable arr[] = sortTestHelper.generateRandomArray(1000000, 1, 100);
+        double time3 = sortTest(mergeSort, arr);
+        System.out.println("MergeSort with randomArray: " + time3 + "s");
     }
 
     private static double sortTest(SortInterface sort , Comparable[] arr) {
---
+++ src/com/imooc/algorithm/MergeSort.java
@@ -0,0 +1,53 @@
+package com.imooc.algorithm;
+
+import java.util.Arrays;
+
+public class MergeSort implements SortInterface {
+
+    public MergeSort() {
+
+    }
+
+    // 将arr[l...mid]和arr[mid+1...r]两部分进行归并
+    private void merge(Comparable[] arr, int l, int mid, int r) {
+        Comparable aux[] = Arrays.copyOfRange(arr, l, r + 1);
+
+        // 初始化，i指向左半部分的起始索引位置l；j指向右半部分起始索引位置mid+1
+        int i = l, j = mid + 1;
+        for (int k = l; k <= r; k++) {
+            if (i > mid) {// 如果左半部分元素已经全部处理完毕
+                arr[k] = aux[j - l];
+                j++;
+            } else if (j > r) {// 如果右半部分元素已经全部处理完毕
+                arr[k] = aux[i - l];
+                i++;
+            } else if (aux[i - l].compareTo(aux[j - l]) < 0) {//左半部分所指元素 < 右半部分所指元素
+                arr[k] = aux[i - l];
+                i++;
+            } else {//右半部分所指元素 < 左半部分所指元素
+                arr[k] = aux[j - l];
+                j++;
+            }
+        }
+    }
+
+    // 递归使用归并排序,对arr[l...r]的范围进行排序
+    private void sort(Comparable[] arr, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+
+        int mid = (r - l) / 2 + l;
+
+        sort(arr, l, mid);
+        sort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+
+    @Override
+    public Object[] sort(Comparable[] arr) {
+        int n = arr.length;
+        sort(arr, 0, n - 1);
+        return arr;
+    }
+}
